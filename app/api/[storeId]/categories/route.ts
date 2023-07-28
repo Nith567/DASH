3d16204d2214b9d4b14d2a37245e4e1414c2669e
Nith567 <91722732+Nith567@users.noreply.github.com>
2023-07-28 16:47:12 +0530

    cors removed

--- app/api/[storeId]/categories/route.ts
+++ app/api/[storeId]/categories/route.ts
@@ -2,44 +2,6 @@ import { NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs';
 import prismadb from '@/lib/prismadb';
 import { Billboard, Category } from '@prisma/client';
-import Cors from 'cors';
-// Initializing the cors middleware with allowed methods
-const corsMiddleware = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-});
-
-async function runMiddleware(req:Request, res:any, fn:any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result:any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
-
-export default async function handler(req:any, res:any) {
-  try {
-    // Run the CORS middleware first
-    await runMiddleware(req, res, corsMiddleware);
-
-    // Now, based on the HTTP method (POST or GET), call the appropriate handler function
-    if (req.method === 'POST') {
-      return await POST(req, req.query);
-    } else if (req.method === 'GET') {
-      return await GET(req, req.query);
-    } else {
-      return new NextResponse("Method not allowed", { status: 405 });
-    }
-  } catch (error) {
-    console.log('[API ERROR]', error);
-    return new NextResponse("Internal error", { status: 500 });
-  }
-}
-
-
-
 
 
 export async function POST(
